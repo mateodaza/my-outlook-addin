d66cdda0cf1a4c1589924a9176abc4eea1ea1d61
mateodaza <mateodaza@outlook.com>
2018-03-09 10:07:54 -0500

    first  commit
    testing appointments

--- src/App.js
+++ src/App.js
@@ -5,7 +5,11 @@ class App extends Component {
   constructor(props) {
     super(props);
 
-    this.onSetColor = this.onSetColor.bind(this);
+    this.state = {
+      subject: ''
+    }
+
+    this.getAppointment = this.getAppointment.bind(this);
   }
 
   onSetColor() {
@@ -16,13 +20,31 @@ class App extends Component {
     });
   }
 
+  getAppointment() {
+    window.Office.context.mailbox.item.subject.getAsync(callback, (response)=>{
+      this.setState({subject: response.value});
+    });
+
+    function callback(asyncResult) {
+      let subject = asyncResult.value;
+      console.log("Subject", subject);
+      return subject;
+      //this.setState({subject: subject});
+    };
+
+  }
+
   render() {
     return (
       <div id="content">
         <div id="content-header">
           <div className="padding">
-              <h1>This is my add-in</h1>
+            <h1>This is my add-in</h1>
           </div>
+        </div>
+        <div id="content-main">
+            <button onClick={this.getAppointment.bind(this)} type="button"> Get Appointment's Body </button>
+            <p> {this.state.subject} </p>
         </div>
       </div>
     );
